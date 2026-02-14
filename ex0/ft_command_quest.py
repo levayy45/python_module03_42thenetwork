9466db8bb8ba7438d4879a75b4bab137c0a72206
levayy <duty9068@gmail.com>
2026-02-14 12:14:45 +0100

    done

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,16 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    if len(sys.argv) <= 1:
+        print("No arguments provided!")
+        print(f"Program name: {sys.argv[0]}")
+    else:
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Arguments received: {len(sys.argv) - 1}")
+        i = 1
+        for arg in sys.argv[1:]:
+            print(f"Argument {i}: ", end="")
+            print(arg)
+            i += 1
+    print(f"Total arguments: {len(sys.argv)}")
