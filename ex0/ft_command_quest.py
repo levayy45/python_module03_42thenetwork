aaa03767f7133e48398aa40b9ee679121f5baf0c
levayy <duty9068@gmail.com>
2026-03-09 18:28:21 +0000

    done

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("No arguments provided!")
         print(f"Program name: {sys.argv[0]}")
+
     else:
         print(f"Program name: {sys.argv[0]}")
         print(f"Arguments received: {len(sys.argv) - 1}")
@@ -13,4 +14,5 @@ if __name__ == "__main__":
             print(f"Argument {i}: ", end="")
             print(arg)
             i += 1
+
     print(f"Total arguments: {len(sys.argv)}")
